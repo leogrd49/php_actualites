c22d9c1e3e40f46beaab12ff7df7e41370a67c95
leogrd49 <leogerard49.pro@gmail.com>
2024-02-28 16:53:18 +0100

    VERY FINISH

--- footer.php
+++ footer.php
@@ -11,10 +11,10 @@
                                         doesn't won't for. Midst life.Over a great night. Green upon years rule ying.</p>
                                 </div>
                                 <div class="footer-social">
-                                    <a href="#"><i class="ti-facebook"></i></a>
-                                    <a href="#"><i class="ti-skype"></i></a>
-                                    <a href="#"><i class="ti-linkedin"></i></a>
-                                    <a href="#"><i class="ti-twitter-alt"></i></a>
+                                    <a href="legal.php"><i class="ti-facebook"></i></a>
+                                    <a href="legal.php"><i class="ti-skype"></i></a>
+                                    <a href="legal.php"><i class="ti-linkedin"></i></a>
+                                    <a href="legal.php"><i class="ti-twitter-alt"></i></a>
                                 </div>
                             </div>
                         </div>
@@ -22,11 +22,11 @@
                             <div class="footer-widget mb-40">
                                 <h3>About Us</h3>
                                 <ul>
-                                    <li><a href="#">Managed Website</a></li>
-                                    <li><a href="#">Power Tools</a></li>
-                                    <li><a href="#">Marketing Service</a></li>
-                                    <li><a href="#">Customer Service</a></li>
-                                    <li><a href="#">Manage Reputation</a></li>
+                                    <li><a href="legal.php">Managed Website</a></li>
+                                    <li><a href="legal.php">Power Tools</a></li>
+                                    <li><a href="legal.php">Marketing Service</a></li>
+                                    <li><a href="legal.php">Customer Service</a></li>
+                                    <li><a href="legal.php">Manage Reputation</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -34,11 +34,11 @@
                             <div class="footer-widget mb-40">
                                 <h3>Quick Links</h3>
                                 <ul>
-                                    <li><a href="#">Store Hours</a></li>
-                                    <li><a href="#">Brand Assets</a></li>
-                                    <li><a href="#">Investor Relations</a></li>
-                                    <li><a href="#">Terms of Service</a></li>
-                                    <li><a href="#">Privacy & Policy</a></li>
+                                    <li><a href="legal.php">Store Hours</a></li>
+                                    <li><a href="legal.php">Brand Assets</a></li>
+                                    <li><a href="legal.php">Investor Relations</a></li>
+                                    <li><a href="legal.php">Terms of Service</a></li>
+                                    <li><a href="legal.php">Privacy & Policy</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -46,11 +46,11 @@
                             <div class="footer-widget mb-40">
                                 <h3>My Account</h3>
                                 <ul>
-                                    <li><a href="#">Press Inquiries</a></li>
-                                    <li><a href="#">Media Directories</a></li>
-                                    <li><a href="#">Investor Relations</a></li>
-                                    <li><a href="#">Terms of Service</a></li>
-                                    <li><a href="#">Speaker Request</a></li>
+                                    <li><a href="legal.php">Press Inquiries</a></li>
+                                    <li><a href="legal.php">Media Directories</a></li>
+                                    <li><a href="legal.php">Investor Relations</a></li>
+                                    <li><a href="legal.php">Terms of Service</a></li>
+                                    <li><a href="legal.php">Speaker Request</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -58,32 +58,35 @@
                             <div class="footer-widget mb-40">
                                 <h3>Resources</h3>
                                 <ul>
-                                    <li><a href="#">Application Security</a></li>
-                                    <li><a href="#">Software Policy</a></li>
-                                    <li><a href="#">Supply Chain</a></li>
-                                    <li><a href="#">Agencies Relation</a></li>
-                                    <li><a href="#">Manage Reputation</a></li>
+                                    <li><a href="legal.php">Application Security</a></li>
+                                    <li><a href="legal.php">Software Policy</a></li>
+                                    <li><a href="legal.php">Supply Chain</a></li>
+                                    <li><a href="legal.php">Agencies Relation</a></li>
+                                    <li><a href="legal.php">Manage Reputation</a></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=IIA%20St%20Nazaire+(IIA)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps trackers</a></iframe></div>
+
             <div class="copyright-area">
                 <div class="container">
                     <div class="copyright-border">
                         <div class="row">
                             <div class="col-xl-6 col-lg-6 col-md-6">
                                 <div class="copyright">
-                                    <p>Copyright © 2019 Xisen. All rights reserved</p>
+                                    <p>Copyright © 2024 Xisen. All rights reserved</p>
                                 </div>
                             </div>
                             <div class="col-xl-6 col-lg-6 col-md-6">
                                 <div class="footer-menu text-left text-md-right">
                                     <ul>
-                                        <li><a href="#">About</a></li>
-                                        <li><a href="#">Tips & Tricks</a></li>
-                                        <li><a href="#">Service</a></li>
+                                        <li><a href="legal.php">About</a></li>
+                                        <li><a href="legal.php">Tips & Tricks</a></li>
+                                        <li><a href="legal.php">Service</a></li>
                                     </ul>
                                 </div>
                             </div>
